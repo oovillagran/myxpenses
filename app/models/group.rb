b9237cadb47e40b5046a2b84479f2cc213b11263
oovillagran <oscar.villagran@hotmail.com>
2023-08-17 03:55:05 -0500

    add tests

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :icon, presence: true
 
   belongs_to :user
   has_many :spendings, dependent: :destroy
